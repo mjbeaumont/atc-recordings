76352fd07cff41bcaa3d19adf4f81555bd3f04ef
Matt Beaumont <mbeaumont@thefarmersdog.com>
2024-10-12 09:55:40 -0400

    updated filename generator to generate necessary data

--- src/filename_generator.py
+++ src/filename_generator.py
@@ -3,8 +3,10 @@ from datetime import datetime, timedelta
 
 class FilenameGenerator:
 
-    INPUT_FORMAT = '%Y-%m-%d %H:%M'
-    OUTPUT_FORMAT = '%b-%d-%Y-%H%MZ'
+    TIME_FORMAT = '%H:%M'
+    INPUT_FORMAT = f'%Y-%m-%d {TIME_FORMAT}'
+    OUTPUT_DATE_FORMAT = '%b-%d-%Y'
+    OUTPUT_FORMAT = f'{OUTPUT_DATE_FORMAT}-{TIME_FORMAT}Z'
     MAIN_URL = 'https://archive.liveatc.net'
 
     def __init__(self, airport, start, num):
@@ -12,24 +14,39 @@ class FilenameGenerator:
         self.start = start
         self.num = num
 
-    def __format_timestamp(self, dt):
-        return dt.strftime(self.OUTPUT_FORMAT)
+    def __get_datetime_string(self, dt, fmt):
+        return dt.strftime(fmt)
 
-    def __generate_timestamps(self):
-        dates = []
+    def __format_datetime(self, dt):
+        return self.__get_datetime_string(dt, self.OUTPUT_FORMAT)
+
+    def __format_date(self, dt):
+        return self.__get_datetime_string(dt, self.OUTPUT_DATE_FORMAT)
+
+    def __format_time(self, dt):
+        return self.__get_datetime_string(dt, self.TIME_FORMAT)
+
+    def __generate_times(self):
+        times = []
         try:
             initial_time = datetime.strptime(self.start, self.INPUT_FORMAT)
-            dates.append(initial_time)
+            times.append(initial_time)
             time_delta = timedelta(minutes=30)
             for _ in range(self.num):
-                next_time = dates[-1] + time_delta
-                dates.append(next_time)
-            return list(map(self.__format_timestamp, dates))
+                next_time = times[-1] + time_delta
+                times.append(next_time)
         except ValueError:
-            print('Invalid date string given as starting point')
+            print(
+                f'{self.start} does not match the format {self.INPUT_FORMAT}'
+            )
+        return times
+
+    def __generate_filename_datecomponent(self):
+        times = self.__generate_times()
+        return list(map(self.__format_datetime, times))
 
     def generate_filenames(self):
-        dates = self.__generate_timestamps()
+        dates = self.__generate_filename_datecomponent()
         files = []
         for date in dates:
             data = {
@@ -37,6 +54,15 @@ class FilenameGenerator:
                 'airport_path': self.airport.lower(),
                 'airport_filename': self.airport.upper()
             }
-            filename = f"{self.MAIN_URL}/{data['airport_path']}/{data['airport_filename']}-{data['date']}.mp3"
-            files.append(filename)
+            filename = f"{data['airport_filename']}-{data['date']}.mp3"
+            url = f"{self.MAIN_URL}/{data['airport_path']}/{filename}"
+            files.append({"filename": filename, "url": url})
         return files
+
+    def generate_combined_filename(self):
+        times = self.__generate_times()
+        airport = self.airport.upper()
+        date = self.__format_date(times[0])
+        start_time = self.__format_time(times[0])
+        end_time = self.__format_time(times[-1])
+        return f'{airport}-{date}-{start_time}-{end_time}Z-combined.mp3'
