858dd5758f5046a916d2643fa963b59c4e661d61
Matt Beaumont <mbeaumont@thefarmersdog.com>
2024-10-14 18:48:42 -0400

    indent

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ def main(
         start: Annotated[str, typer.Argument(help='YYYY-mm-dd H:I (in UTC)', prompt=True)],
         feed: Annotated[Optional[str], typer.Option(help='Defaults to airport if not provided', prompt=True)],
         number_of_periods: Annotated[Optional[int], typer.Option(help='How many 30 minute periods to include')] = 4
-    ):
+        ):
     feed_name = feed if feed else airport.upper()
 
     fg = FilenameGenerator(
