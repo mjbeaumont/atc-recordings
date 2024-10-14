fdaae88a570116a14866567800678f4abfcb7573
Matt Beaumont <mbeaumont@thefarmersdog.com>
2024-10-14 18:47:45 -0400

    added typer for CLI support

--- src/main.py
+++ src/main.py
@@ -1,9 +1,30 @@
+from typing import Optional
 from filename_generator import FilenameGenerator
 from  filemanager import FileManager
+import typer
+from typing_extensions import Annotated
 
-fg = FilenameGenerator('kcdw', 'KCDW', '2024-10-10 1200', 4) 
-urls = fg.generate_filenames()
-combined_filename = fg.generate_combined_filename()
 
-fm = FileManager(urls, combined_filename)
-fm.process_recordings()
+def main(
+        airport: Annotated[str, typer.Argument(help='4-letter ICAO code')],
+        start: Annotated[str, typer.Argument(help='YYYY-mm-dd H:I (in UTC)', prompt=True)],
+        feed: Annotated[Optional[str], typer.Option(help='Defaults to airport if not provided', prompt=True)],
+        number_of_periods: Annotated[Optional[int], typer.Option(help='How many 30 minute periods to include')] = 4
+    ):
+    feed_name = feed if feed else airport.upper()
+
+    fg = FilenameGenerator(
+        airport.lower(),
+        feed_name,
+        start,
+        number_of_periods
+    )
+    urls = fg.generate_filenames()
+    combined_filename = fg.generate_combined_filename()
+
+    fm = FileManager(urls, combined_filename)
+    fm.process_recordings()
+
+
+if __name__ == "__main__":
+    typer.run(main)
